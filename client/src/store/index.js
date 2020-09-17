20c243047201170bf26ef4144f3bc3ca900d59cd
bryandaniswara <bryan.daniswara@gmail.com>
2020-09-18 00:39:52 +0700

    Feature: Set username & lobby

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,14 +1,26 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import router from '../router'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    username: '',
   },
   mutations: {
+    setUsername(state, username) {
+      state.username = username;
+    },
   },
   actions: {
+    SOCKET_firstTurn({ state }, payload) {
+      if (state.username === payload) {
+        router.push({ name: 'Play' })
+      } else {
+        router.push({ name: 'Waiting' })
+      }
+    }
   },
   modules: {
   }
