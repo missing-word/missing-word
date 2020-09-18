1d250dfb342b60cc071f6a910b6e08a30edc4c0e
abdulfattah94 <abdulfattah.1994@gmail.com>
2020-09-18 13:54:52 +0700

    fixed server

--- server/app.js
+++ server/app.js
@@ -1,31 +1,31 @@
 var app = require("express")();
 var http = require("http").createServer(app);
 var io = require("socket.io")(http);
-const port = 3000;
+const port = 3001;
 
 app.get("/", (req, res) => {
   res.send(`ready to start`);
 });
 let player = [];
-let counter = 0;
+let matchRound = 0;
 let message = "";
 let playerFirst = "";
 io.on("connection", (socket) => {
-  console.log("a user connected");
-  if (counter === 4) {
-    console.log("selesai");
-  }
+  console.log("a user connected")
   socket.on("ready", (payload) => {
     if (player.length > 1) {
       player.shift();
     }
     player.push(payload);
     console.log(player);
+    if(player.length === 2){
+      io.emit('startGame', true)
+    }
   });
   socket.on("start", () => {
     playerFirst += player[1].username;
     console.log(playerFirst, "first turn");
-    socket.emit("firstTurn", playerFirst);
+    io.emit("firstTurn", playerFirst);
     playerFirst = "";
   });
   socket.on("notif", () => {
@@ -52,7 +52,6 @@ io.on("connection", (socket) => {
     console.log(trueWord);
     socket.broadcast.emit("trueWord", trueWord);
     socket.broadcast.emit("missingWord", missArr);
-    counter++;
   });
   socket.on("answer", (payload) => {
     console.log(payload);
@@ -63,7 +62,8 @@ io.on("connection", (socket) => {
         if (el.username === payload.username) {
           el.points += 10;
           socket.emit("getPoint", el);
-          console.log(counter);
+          socket.broadcast.emit('benar', 'Unfortunately! Your rival hit the right answer!')
+          socket.emit('notifBenar', 'Yeay! You hit the right answer, you got 10 points!')          
           word = "";
           break;
         }
@@ -73,16 +73,42 @@ io.on("connection", (socket) => {
         if (el.username !== payload.username) {
           el.points += 10;
           socket.broadcast.emit("getPoint", el);
+          socket.emit('salah', 'Unfortunately! You hit the wrong answer :(')
+          socket.broadcast.emit('notifSalah', 'Yeay! Your rival hit the wrong answer, you got 10 points!')
           word = "";
           break;
         }
       }
     }
+    matchRound++
+    console.log(player);
+    console.log(matchRound, '<<< game round');
+    io.emit('changeTurn', payload.username)
+    if(matchRound == 2){
+      let points = 0
+      let winner = ''
+      for(const el of player){
+        if(el.points > points){
+          winner = el.username
+        }
+      }
+      if(winner === payload.username){
+        socket.emit('gameOver', 'Game Over! You are the winner, yeay!')
+        socket.broadcast.emit('gameOver', `Game Over! ${winner} is the winner!`)
+      }else{
+        socket.emit('gameOver', `Game Over! ${winner} is the winner!`)
+        socket.broadcast.emit('gameOver', `Game Over! You are the winner, yeay!`)
+      }
+      player = [];
+      matchRound = 0;
+      console.log(player, matchRound);
+    }
+
   });
   socket.on("disconnect", (reason) => {
     if (reason === "io client disconnect") {
       player = [];
-      counter = 0;
+      matchRound = 0;
     }
   });
 });
