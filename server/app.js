b992d7c5ce2aa4c415f28bbf88d3ce06c81e3dce
rrizkie <rizkieawanj@gmail.com>
2020-09-18 07:16:31 +0700

    server new update

--- server/app.js
+++ server/app.js
@@ -1,60 +1,90 @@
 var app = require('express')();
 var http = require('http').createServer(app);
-var io = require('socket.io')(http);
-
-
-
-
+var io = require('socket.io')(http)
+const port = 3001
 app.get('/', (req, res) => {
   res.send(`ready to start`);
 });
-
-let player = []
-let counter = 1
-const timeout = 1000
-let message = ''
-let playerFirst = ''
-
-
+  let player = []
+  let counter = 0
+  let message = ''
+  let playerFirst = ''
 io.on('connection', (socket) => {
   console.log('a user connected');
-
+  if(counter === 4){
+    console.log('selesai')
+  }
   socket.on('ready',(payload)=>{
+    if(player.length > 1){
+      player.shift()
+    }
     player.push(payload)
-
-    if (player.length === 2){
-      playerFirst += player[Math.round(Math.random()*2)].username
-      socket.emit('firstTurn',playerFirst)
-    }else{}
+    console.log(player)
   })
-
+  socket.on('start',()=>{
+      playerFirst += player[1].username
+      console.log(playerFirst, 'first turn')
+      socket.emit('firstTurn', playerFirst)
+      playerFirst = ''
+  })
+  socket.on('notif',() => {
+    console.log('masuk notif')
+    socket.broadcast.emit('notif', 'false')
+  })
   socket.on('word',(word)=>{
-    message += word
+    message = word.toUpperCase()
     let wordArr = word.split('')
-    let sendArr = []
+    const trueWord = []
+    const missArr = []
     let count = 0
     for (let i = 0 ; i< wordArr.length ;i++){
+      trueWord.push(wordArr[i].toUpperCase())
       let random = Math.round(Math.random())
       if (random === 1 && count < 3){
-        sendArr.push('_')
+        missArr.push('_')
         count++
       }else{
-        sendArr.push(wordArr[i])
+        missArr.push(wordArr[i].toUpperCase())
       }
     }
-    socket.broadcast.emit('missingWord',sendArr)
+    console.log(missArr)
+    console.log(trueWord)
+    socket.broadcast.emit('trueWord', trueWord)
+    socket.broadcast.emit('missingWord', missArr)
     counter++
   })
   socket.on('answer',(payload)=>{
-    if (message === payload){
-      
+    console.log(payload)
+    console.log(message === payload.message.toUpperCase())
+    if (message === payload.message.toUpperCase()){
+      console.log(player)
+      for(const el of player){
+        if(el.username === payload.username){
+          el.points += 10
+          socket.emit('getPoint', el)
+          console.log(counter)
+          word = ''
+          break
+        }
+      }
+    } else {
+      for(const el of player){
+        if(el.username !== payload.username){
+          el.points += 10
+          socket.broadcast.emit('getPoint', el)
+          word = ''
+          break
+        }
+      }
     }
   })
+  socket.on('disconnect', (reason) => {
+    if (reason === 'io client disconnect') {
+      player = []
+      counter = 0
+  }
+  });
 });
-
-http.listen(3000, () => {
-  console.log('listening on *:3000');
+http.listen(port, () => {
+  console.log(`listening on *:${port}`);
 });
-
-
-
